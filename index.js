64ea81a674147f08180ef8328e1bf2fb29d77ef1
Ar Zh <job.ar.zh@gmail.com>
2017-09-29 12:29:21 +0600

    Moved 'fortune cookie' functionality into module.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune');
 
 var app = express();
 
@@ -15,8 +16,7 @@ app.get('/', function (reg, res) {
     res.render('home');
 });
 app.get('/about', function (req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 app.use(function (req, res, next) {
@@ -33,11 +33,3 @@ app.listen(app.get('port'), function () {
     console.log('Express запущен на http://localhost:' +
     app.get('port') + '; нажмите Ctrl + C для завершения');
 });
-
-var fortunes = [
-    "Победи свои страхи, или они победят тебя.",
-    "Рекам нужны истоки.",
-    "Не бойся неведомого.",
-    "Тебя ждет приятный сюрприз.",
-    "Будь проще везде, где только можно."
-];
